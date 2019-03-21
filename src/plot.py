14162cf7af2d0f5cf05c13a70eddc22f62acfbb3
haradayu <>
2019-03-21 09:15:42 +0900

    plot heatmap

--- src/plot.py
+++ src/plot.py
@@ -1,14 +1,15 @@
 import argparse
 import numpy as np; np.random.seed(0)
-import seaborn as sns; sns.set()
+import matplotlib.pyplot as plt
+import seaborn as sns
 
 parser = argparse.ArgumentParser(description='ログファイルからヒートマップを作成します')
 parser.add_argument('input', type=str,
                     help='ログファイル')
 args = parser.parse_args()
 
-lr_value_list = [0.0005, 0.001, 0.002, 0.004, 0.008, 0.016, 0.032, 0.064,0.128, 0.256, 0.512]
-batchsize_value_list = [2, 4, 8, 16, 32, 64, 128, 256, 512, 1024]
+lr_value_list = [0.0005, 0.001, 0.002, 0.004, 0.008, 0.016, 0.032, 0.064,0.128, 0.256, 0.512]#縦
+batchsize_value_list = [2, 4, 8, 16, 32, 64, 128, 256, 512, 1024]#横
 dataset =  np.zeros((len(lr_value_list), len(batchsize_value_list)))
 with open(args.input,"r") as f:
     lr = 0
@@ -27,3 +28,5 @@ with open(args.input,"r") as f:
             if dataset[lr_index, batchsize_index] < acc:   
                 dataset[lr_index, batchsize_index] = acc
 print(dataset)
+sns.heatmap(dataset, annot=True, fmt="f")
+plt.show()
